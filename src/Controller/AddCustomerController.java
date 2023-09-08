b0fb5f864b861a78d56ed89311ac3c958b013879
Winslyc <wcyrius1@gmail.com>
2023-09-08 15:51:10 -0400

    Added Customer Controller and Add Customer Controller Page.

---
+++ src/Controller/AddCustomerController.java
@@ -0,0 +1,11 @@
+package Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+
+
+public class AddCustomerController {
+    @FXML
+    TextField idTextField;
+}
--- src/Controller/CustomerController.java
+++ src/Controller/CustomerController.java
@@ -1,4 +1,38 @@
 package Controller;
 
-public class CustomerController {
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CustomerController implements Initializable {
+    @FXML
+    protected Button addButton;
+
+
+
+    public void onClickAddButton(ActionEvent actionEvent) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource("/View/AddCustomer.fxml"));
+        Scene scene = new Scene(parent);
+        Stage stage = (Stage)((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
